3c7add43ea8b8a390c45fec06a081c14847eb418
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-06-28 19:26:17 +0300

    added the new route for JSON

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title string `json:"Title"`
+	Dec string `json:"desc"`
+	Content string `json:"content"`
+
+}
+type Articles []Article
+
+func allArticles (w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title:"Test Title", Dec:"Test Description", Content: "Hello World"},
+	}
+	fmt.Println("EndPoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homPage (w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hompage Endpoint Hit")
@@ -12,6 +29,7 @@ func homPage (w http.ResponseWriter, r *http.Request) {
 
 func handleRequests () {
 	http.HandleFunc("/", homPage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
